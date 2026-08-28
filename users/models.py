d4b62f0d529a92ac5277dc3a0124b7c66ee01089
Erik Dias <erikdiasbardock@gmail.com>
2026-08-27 22:46:59 -0300

    estrutura inicial do Dia Fit

---
+++ users/models.py
@@ -0,0 +1,24 @@
+#traz para o arquivo as ferramentas que usaremos para criar campos e modelos do banco.
+#traz a estrutura de usuário que o próprio Django já criou.
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+
+    # Tipos de usuário permitidos no Dia Fit.
+    TIPO_ALUNO = 'ALUNO'
+    TIPO_PROFESSOR = 'PROFESSOR'
+
+
+    TIPOS_USUARIO = [
+        (TIPO_ALUNO, 'Aluno'),
+        (TIPO_PROFESSOR, 'Professor'),
+    ]
+
+    tipo_usuario = models.CharField(
+        max_length=10,
+        choices=TIPOS_USUARIO,
+        default=TIPO_ALUNO,
+    )
